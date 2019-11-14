cadd3315c48d276b45f013bb969a7258bf0ec9a6
shoooone <pride.a.s.w.12.01@gmail.com>
2019-11-14 16:46:24 +0900

    eslint + prettier設定追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,19 @@ module.exports = {
   env: {
     node: true,
   },
+  plugins: ['prettier'],
   extends: [
-    'plugin:vue/essential',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/vue',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',

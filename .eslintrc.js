935be7df531c53be4ba45cb0d0eb61c0be6710f0
shoooone <pride.a.s.w.12.01@gmail.com>
2019-11-18 17:50:06 +0900

    eslint設定修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-shadow': 'off',
     'prettier/prettier': 'error',
+    'class-methods-use-this': 'off',
   },
   parserOptions: {
     parser: '@typescript-eslint/parser',

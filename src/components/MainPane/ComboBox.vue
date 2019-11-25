b7e7513e8199cd8e148842e9cea48aedd8251ce4
shoooone <pride.a.s.w.12.01@gmail.com>
2019-11-25 21:58:52 +0900

    コンボボックス

---
+++ src/components/MainPane/ComboBox.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="combo-box">
+    <select id="" name="combo-box">
+      <option v-for="option in options" :key="option.name" :value="option.value">
+        {{ option.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+export interface Option {
+  name: string;
+  value: string;
+}
+
+@Component
+export default class ComboBox extends Vue {
+  @Prop()
+  options!: Option[];
+}
+</script>
+
+<style scoped></style>
